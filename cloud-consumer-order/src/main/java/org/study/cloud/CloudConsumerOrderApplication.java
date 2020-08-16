69ea4f2ef98cfd0e6b677a3dd58c1fcde16403a4
AsaLearner <18812139572@163.com>
2020-08-16 23:16:52 +0800

    oder设置ribbon负载均衡

--- cloud-consumer-order/src/main/java/org/study/cloud/CloudConsumerOrderApplication.java
+++ cloud-consumer-order/src/main/java/org/study/cloud/CloudConsumerOrderApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.study.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
 //@EnableDiscoveryClient
+@RibbonClient(name = "CLOUD-PAYMENT-SERVER" , configuration = MySelfRule.class)
 public class CloudConsumerOrderApplication {
 
     public static void main(String[] args) {
