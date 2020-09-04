26be91d2bde26fb1e556bf3c99bfd7631bf1cf74
AsaLearner <18812139572@163.xom>
2020-09-04 16:38:26 +0800

    bus+config

---
+++ cloud-config-client-3366/src/main/java/cloud/config/client3366/CloudConfigClient3366Application.java
@@ -0,0 +1,15 @@
+package cloud.config.client3366;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudConfigClient3366Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClient3366Application.class, args);
+    }
+
+}
---
+++ cloud-config-client-3366/src/main/java/cloud/config/client3366/controller/ConfigController.java
@@ -0,0 +1,22 @@
+package cloud.config.client3366.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value(("${server.port}"))
+    private String port;
+
+    @GetMapping("/configinfo")
+    public String getConfigInfo(){
+        return configInfo + "\n\n 端口："+port;
+    }
+}
